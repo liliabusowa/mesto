1df661ea741102e10deda814e2bebb499efa901c
Lilia Busowa <liliabusowa@yandex.ru>
2022-01-24 12:48:44 +0700

    template string

--- scripts/index.js
+++ scripts/index.js
@@ -27,3 +27,53 @@ function savePopup(evt) { //сохранение попапа
 openButton.addEventListener('click', openPopup)
 closeButton.addEventListener('click', closePopup)
 formElement.addEventListener('submit', savePopup); 
+
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  const list = document.querySelector('.elements');
+
+  const createHTMLString = (item) => {
+      return `<article class="element">
+      <button type="button" class="element__trash"></button>
+      <img 
+        class="element__photo" 
+        src="${item.link}" 
+        alt="Карачаевск">
+            <div class="element__description">
+                <h2 class="element__mesto">${item.name}</h2>
+                <button type="button" class="element__like element__like_active"></button>
+            </div>
+      </article>`;
+  }
+
+  const result = initialCards.map((item) => {
+      return createHTMLString(item);
+  });
+
+  list.insertAdjacentHTML('afterbegin', result.join(''));
