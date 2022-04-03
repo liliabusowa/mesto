602659864d2dc2203317761657b596c934add796
Lilia Busowa <liliabusowa@yandex.ru>
2022-04-04 03:16:29 +0700

    8.2

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,16 @@
 export default class Section {
-    constructor({ initialCards: items, renderer }, containerSelector) {
-      this._items = items;
-      this._renderer = renderer;
-      this._containerSelector = containerSelector;
-    }
-    renderItems() {
-      this._items.forEach(item => {
-        const element = this._renderer(item);
-        this.addItem(element);
-      });
-    }
-    addItem(element) {
-      document.querySelector(this._containerSelector).prepend(element);
-    }
+  constructor({ initialCards: items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._containerSelector = containerSelector;
   }
+  renderItems() {
+    this._items.forEach(item => {
+      const element = this._renderer(item);
+      this.addItem(element);
+    });
+  }
+  addItem(element) {
+    document.querySelector(this._containerSelector).prepend(element);
+  }
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,57 +1,56 @@
 // Массив карточек для первоначального заполнения галереи при загрузке страницы
 export const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-    },
-  ];
-  
-  // Объект настроек с селекторами и классами элементов, используемых при валидации
-  export const validationConfig = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__save-button',
-    invalidInputClass: 'popup__input_state_invalid',
-    errorClass: 'popup__input-error_active',
-    submitButtonClass: 'popup__save-button_inactive',
-  };
-  
-  // Селектор контейнера для добавления новых карточек
-  export const cardsContainerSelector = '.gallery__list';
-  
-  // Селектор шаблона карточки
-  export const cardTemplateSelector = '.card-template';
-  
-  // Селектор попапа создания карточки
-  export const cardPopupSelector = '.popup_type_card';
-  
-  // Селектор попапа профиля
-  export const profilePopupSelector = '.popup_type_profile';
-  
-  // Селектор попапа просмотра фото
-  export const pfotoPopupSelector = '.popup_type_image';
-  
-  // Селекторы элементов профиля пользователя
-  export const userNameElementSelector = '.profile__name';
-  export const userDescriptionSelector = '.profile__job';
-  
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+// Объект настроек с селекторами и классами элементов, используемых при валидации
+export const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__save-button',
+  invalidInputClass: 'popup__input_state_invalid',
+  errorClass: 'popup__input-error_active',
+  submitButtonClass: 'popup__save-button_inactive',
+};
+
+// Селектор контейнера для добавления новых карточек
+export const cardsContainerSelector = '.gallery__list';
+
+// Селектор шаблона карточки
+export const cardTemplateSelector = '.card-template';
+
+// Селектор попапа создания карточки
+export const cardPopupSelector = '.popup_type_card';
+
+// Селектор попапа профиля
+export const profilePopupSelector = '.popup_type_profile';
+
+// Селектор попапа просмотра фото
+export const pfotoPopupSelector = '.popup_type_image';
+
+// Селекторы элементов профиля пользователя
+export const userNameElementSelector = '.profile__name';
+export const userDescriptionSelector = '.profile__job';
